714511a168f8b09c359af4d7f3fe81c6830b1913
snatesh <sachin.natesh@illinoisrocstar.com>
2017-08-24 21:50:02 -0500

    done except output

--- utils/vt_mesh_map.C
+++ utils/vt_mesh_map.C
@@ -1,13 +1,14 @@
+//standard
 #include <iostream>
+// third party
 #include <vtkAnalyzer.H>
-
 
 int main(int argc, char* argv[])
 {
-  if (argc < 3)
+  if (argc < 4)
 	{
 		std::cerr << " VTK 3D-2D mesh transfer utility" << std::endl
-							<< " Usage: " << argv[0] << " VolMeshFile PlaneMeshFile" 
+							<< " Usage: " << argv[0] << " VolMeshFile PlaneMeshFile species_name" 
 							<< std::endl;
 		exit(1);
 	}
@@ -25,32 +26,50 @@ int main(int argc, char* argv[])
 	std::cout << std::endl;
 	PlaneMesh->report();
 	
-	/*unsigned long num_points = PlaneMesh->getNumberofPoints();
-	
-	for (int i = 0; i < num_points; ++i) {
-		double* coord = PlaneMesh->getPointCoords(i);
-			
-
-		std::cout << coords[0] << " " << coords[1] << " "
-							<< coords[2] << std::endl;
-	}	*/
-
 	int numComponent;	
-	unsigned long num_cells = PlaneMesh->getNumberOfCells();
-	for (int i = 0; i < num_cells; ++i) {
-	//	PlaneMesh->getCellCoords(i);}
-		double** cellCoords = PlaneMesh->getCellCoords(i,numComponent);
-		for (int j = 0; j < numComponent ; ++j) {
-			std::cout << cellCoords[j][0] << " " << cellCoords[j][1] 
-								<< " " << cellCoords[j][2] << std::endl;
-			free(cellCoords[j]);
+	//int num_plane_cells = PlaneMesh->getNumberOfCells();
+	int num_plane_points = PlaneMesh->getNumberOfPoints();
+	int num_vol_points = VolMesh->getNumberOfPoints();
+	int nDim = 3;
+
+	// get centers of cells in plane mesh
+	std::vector<double> PlaneCellCenters = 
+	PlaneMesh->getCellCenters(numComponent, nDim);
+
+	//get coordinates of points of cells in vol mesh
+	std::vector<double> VolPointCoords = 
+	VolMesh->getAllPointCoords(nDim);		
+
+	// TODO: HOW MANY NEIGHBORS?
+
+	// creating interpolation for cross_link 
+	// cross_link 'species' name should be passed by cmdline
+	int species_id = VolMesh->IsArrayName(std::string(argv[3]));
+	if (species_id >= 0) {
+		basicInterpolant* VolPointInterp = 
+			new basicInterpolant(nDim, num_vol_points, 10, VolPointCoords);
+		std::vector<std::vector<double>> volDataMat;
+		int numTuple, numComponent;
+		VolMesh->getPointDataArray(species_id, volDataMat, numTuple, numComponent);
+		for (int j = 0; j < numComponent; ++j) {
+			std::vector<double > volData(numTuple);
+			for (int i = 0; i < numTuple; ++i) {
+					volData[i] = volDataMat[i][j];
 			}
-		std::cout << std::endl;
-		free(cellCoords);
+			std::vector<double> interpData;
+			VolPointInterp->interpolate(PlaneCellCenters.size()/nDim, 
+																	PlaneCellCenters, volData, interpData);
+			
+			for (int i = 0; i < PlaneCellCenters.size()/nDim; ++i)
+				std::cout << interpData[i] << std::endl;			
+
+		}
 	}
+
+
 
 	delete VolMesh;
 	delete PlaneMesh;
-	return 0;
+	return EXIT_SUCCESS;
 }
 
