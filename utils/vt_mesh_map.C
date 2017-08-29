e895469f2956240497a87193b4f8bf9929a519d9
snatesh <sachin.natesh@illinoisrocstar.com>
2017-08-28 23:03:57 -0500

    update

--- utils/vt_mesh_map.C
+++ utils/vt_mesh_map.C
@@ -2,6 +2,12 @@
 #include <iostream>
 // third party
 #include <vtkAnalyzer.H>
+#include <ModelInterface.h>
+#include <MAdLib.h>
+#include <NodalDataManager.h>
+#include <MeshDataBaseIO.h>
+#include <CheckOrientation.h>
+#include <GmshEntities.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,10 +15,36 @@ int main(int argc, char* argv[])
 	{
 		std::cerr << " VTK 3D-2D mesh transfer utility" << std::endl
 							<< " Usage: " << argv[0] << 
-									" VolMeshFile PlaneMeshFile species_name [write_coords=0|1] [outfile]" 
+									" VolVTKFile PlaneVTKFile species_name [write_coords=0|1] [outfile]" 
 							<< std::endl;
 		exit(1);
 	}
+
+
+	// TODO: Testing gmsh/madlib stuff
+	if (argc == 7) { 
+		std::string mshFileName = argv[6];
+		// Reading gmsh file and info
+  	MAd::pGModel model = NULL;
+		MAd::GM_create(&model,"initial");
+  	MAd::pMesh mesh = MAd::M_new(model);
+  	MAd::M_load(mesh, mshFileName.c_str());
+		std::cout << "Using tags?: " << MAd::GM_physical(model) << std::endl;
+	
+		std::cout << model.getNumVertices() << std::endl;
+
+		/*std::vector<std::vector<double>> curds;
+		MAd::VIter vit = M_vertexIter(mesh);
+		MAd::pVertex pv;
+		while (pv = VIter_next(vit)) {
+			MAd::pPoint pp = MAd::V_point(pv);
+			std::cout << pp->X << " " << pp->Y << " " << pp->Z << std::endl;
+		}*/
+	
+		MAd::checkMesh(mesh);
+  	MAd::M_info(mesh);
+	}
+
 
 	// Parsing args and Reading files from command line
 	vtkAnalyzer* VolMesh;
@@ -22,16 +54,6 @@ int main(int argc, char* argv[])
 	VolMesh->read();
 	PlaneMesh->read();
 
-	
-	
-	// Report mesh statistics
-//	std::cout << "Reporting on VolMesh:" << std::endl;
-//	VolMesh->report();
-//	std::cout << std::endl;
-//	std::cout << "Reporting on PlaneMesh:" << std::endl;
-//	PlaneMesh->report();
-//	std::cout << std::endl;
-	
 	int numComponent;	
 	int num_plane_points = PlaneMesh->getNumberOfPoints();
 	int num_vol_points = VolMesh->getNumberOfPoints();
@@ -45,7 +67,8 @@ int main(int argc, char* argv[])
 
 	// creating interpolation for cross_link 
 	// cross_link 'species' name should be passed by cmdline
-	int species_id = VolMesh->IsArrayName(std::string(argv[3]));
+	//int species_id = VolMesh->IsArrayName(std::string(argv[3]));
+	int species_id = VolMesh->IsArrayName(argv[3]);
 	if (species_id >= 0) {
 		std::vector<std::vector<double>> volDataMat;
 		int numTuple, numComponent;
@@ -58,13 +81,13 @@ int main(int argc, char* argv[])
 		if (argc >= 5) { 
 			int write_coord = atoi(argv[4]);
 			switch(write_coord) {
-				case 0: { if (argc == 6)
+				case 0: { if (argc >= 6)
 										VolMesh->writeInterpData(interpData, argv[5]);
 									else
 										VolMesh->writeInterpData(interpData, std::cout);
 									break;
 								}
-				case 1: { if (argc==6)
+				case 1: { if (argc >= 6)
 										VolMesh->writeInterpData(interpData, PlaneCellCenters, nDim, argv[5]);
 									else
 										VolMesh->writeInterpData(interpData, PlaneCellCenters, nDim, std::cout);
@@ -84,7 +107,6 @@ int main(int argc, char* argv[])
 		std::cerr << "No point data arrays found in " << argv[1] << std::endl;
 		exit(1);
 	}
-
 
 
 	delete VolMesh;
