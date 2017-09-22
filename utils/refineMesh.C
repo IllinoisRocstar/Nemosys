65679af1cc98cb9140eecc674a596d3c60bf5beb
snatesh <sachin.natesh@illinoisrocstar.com>
2017-09-21 23:48:21 -0500

    edit

--- utils/refineMesh.C
+++ utils/refineMesh.C
@@ -4,23 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-  /*MAd::pGModel gmodel = NULL;
-  MAd::GM_create(&gmodel, "GeoModel");
-  MAd::GM_read(gmodel, argv[1]);
-  
-  MAd::pMesh mesh = MAd::M_new(gmodel);
-  MAd::M_load(mesh, argv[2]); 
-  
-  MAd::PWLSField* sizeField = new MAd::PWLSField(mesh);
-  
-  sizeField->setCurrentSize();
-  sizeField->scale(0.5);
-  
-  MAd::MeshAdapter* adapter = new MAd::MeshAdapter(mesh, sizeField);
-  adapter->run();
 
-  MAd::M_writeMsh(mesh, "newMesh.msh", 2);
-  */
 
    
 
@@ -86,9 +70,26 @@ int main(int argc, char* argv[])
 
   srcGModel->writeMSH("vtkConverted.msh", 2.2);*/
 
-  std::string ofname = "testmsh.msh";
+  std::string ofname = "converted.msh";
   tmpmesh->writeMSH(ofname);
  
+  MAd::pGModel gmodel = NULL;
+  //MAd::GM_create(&gmodel, "GeoModel");
+  //MAd::GM_read(gmodel, argv[1]);
+  
+  MAd::pMesh mesh = MAd::M_new(gmodel);
+  MAd::M_load(mesh, "converted.msh"); 
+  
+  MAd::PWLSField* sizeField = new MAd::PWLSField(mesh);
+  
+  sizeField->setCurrentSize();
+  sizeField->scale(0.5);
+  
+  MAd::MeshAdapter* adapter = new MAd::MeshAdapter(mesh, sizeField);
+  adapter->run();
+  //apater->removeSlivers();
+  MAd::M_writeMsh(mesh, "refined.msh", 2);
+  
   return 0;
 }
 
