a308a9a427331393adef46771ccdd858756d551b
snatesh <sachin.natesh@illinoisrocstar.com>
2017-10-24 13:11:23 -0500

    testing gradient size field

--- utils/refineMesh.C
+++ utils/refineMesh.C
@@ -8,13 +8,14 @@
 int main(int argc, char* argv[])
 {
   // Check input
-  if ( argc != 3 ) {
-    std::cout << "Usage Error: " << argv[0] << " meshFile outputMesh" << std::endl;
+  if ( argc != 5 ) {
+    std::cout << "Usage Error: " << argv[0] << " meshFile outputMesh array_id size" << std::endl;
     exit(1);
   }
   std::string meshFile = argv[1];
 
 
+  // ------------------- MESHPHYS TESTS ----------------------------//
   std::cout << "TESTING MESHPHYS CLASS" << std::endl; 
   meshPhys* mshphys;
   mshphys = new meshPhys((char*) &(meshFile)[0u]);
@@ -34,20 +35,152 @@ int main(int argc, char* argv[])
     if (i > 50)
       break;
   } */ 
+
+  int array_id = std::stoi(argv[3]);
+  double size = std::stof(argv[4]); 
  
-  std::cout << "Gradient Test: \n"; 
-  std::vector<double> gradient = mshphys->ComputeGradAtCell(1000,0);
-  for (int i =0; i < 9; ++i)
-  {
-    std::cout << gradient[i] << " ";
-    if ((i+1)%3 == 0)
-      std::cout << std::endl;
-  }  
+  std::vector<double> gradients = mshphys->ComputeL2GradAtAllCells(array_id);
+  reciprocal_vec(gradients); 
+  std::vector<double> lengths = mshphys->GetCellLengths();
+  std::vector<double> gradminmax = getMinMax(gradients);
+  std::vector<double> lengthminmax = getMinMax(lengths);
+  std::cout << "Gradient and Length Tests: \n";
+  for (int i = 0; i < mshphys->numberOfCells; ++i)
+  {   
+    std::cout << lengths[i] << " " << gradients[i] <<  std::endl;
+  }
+
+  lengthminmax[1] -= (lengthminmax[1] - lengthminmax[0])/2.0;
+  std::cout << "\n \n Minmax Lengths " << lengthminmax[0] << " "
+            << lengthminmax[1] << std::endl;  
+
+  std::cout << "\n \n Minmax Gradients " << gradminmax[0] << " "
+            << gradminmax[1] << std::endl;  
+
+  scale_vec_to_range(gradients, gradminmax, lengthminmax); 
+  std::vector<double> scalesminmax = getMinMax(gradients);
+  std::cout << "Scale Min and Max: ";
+  std::cout << scalesminmax[0] << " " << scalesminmax[1] << std::endl;
+  for (int i = 0; i < mshphys->numberOfCells; ++i)
+    std::cout << gradients[i] << std::endl;  
   
-  std::cout << "\n \n \n ";
-  mshphys->ComputeGradAtPoint(7000,0);
+
+  mshphys->getCellsWithPoint(10);
+  
   mshphys->report(); 
+ // delete mshphys;
+  //--------------------------------- END MESHPHYS TESTS ----------------------//
+
+  // create gmodel from input mesh
+  MAd::pGModel gmodel = 0;
+  MAd::pMesh mesh = MAd::M_new(gmodel);
+  vtkAnalyzer* tmpmesh;
+  if (meshFile.find(".v") != -1 ) 
+  {
+    tmpmesh = new vtkAnalyzer((char*) &(meshFile)[0u]);
+    tmpmesh->read();
+    std::string ofname = "converted.msh";
+    tmpmesh->writeMSH(ofname);
+
+    MAd::M_load(mesh, "converted.msh"); 
+  }
+  else if (meshFile.find(".msh") != -1)
+  {
+    MAd::M_load(mesh, (char*) &(meshFile)[0u]); 
+  }
+
+  // find 2d boundary elements
+  MAd::pGEntity bnd = (MAd::pGEntity) MAd::GM_faceByTag(mesh->model, 0);
+  mesh->classify_grid_boundaries(bnd);
+  mesh->classify_unclassified_entities();
+  mesh->destroyStandAloneEntities();
+
+  // writing background mesh and taking care of non tet/tri entities
+  mshphys->writeBackgroundMSH("backgroundSF.msh", gradients);
   delete mshphys;
+  MAd::BackgroundSF* bSF = new MAd::BackgroundSF("backgroundSF");
+  bSF->loadData("backgroundSF.msh");
+  std::cout << "\n Beginning Adapter Construction \n" << std::endl;
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+  MAd::MeshAdapter* adapter = new MAd::MeshAdapter(mesh, bSF);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << "Time for adapter construction (ms): " 
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() 
+            << std::endl;
+  
+  /* TODO: writing vector data not implemented in madlib
+           instead, split vector data into component vectors
+           if writing data required */  
+
+
+  // retrieving point data from vtk mesh
+  std::vector<std::vector<double>> pntData;
+  int numTuple, numComponent; 
+  tmpmesh->getPointDataArray(array_id, pntData, numTuple, numComponent);
+
+  // attaching point data to adapter's mesh
+  adapter->registerVData("displacement", pntData);
+  // check if data is correctly attached
+  std::vector<std::vector<double>> preAMRData;
+  adapter->getMeshVData("displacement", &preAMRData);
+  std::cout << "preAMRData[1][1] = " << preAMRData[1][1] << std::endl;
+  std::cout << "Size of vector = " << preAMRData.size() << std::endl; 
+
+  // Output situation before optimization
+  std::cout << "Statistics before optimization: " << std::endl;
+  adapter->printStatistics(std::cout);
+  
+  // Optimize
+  std::cout << "Optimizing the mesh ..." << std::endl;
+  adapter->run();
+  adapter->removeSlivers();
+
+  // Outputs final mesh
+  std::cout << "Statistics after optimization: " << std::endl;
+  adapter->printStatistics(std::cout);
+
+  // writing refined mesh to file in msh format 
+  MAd::M_writeMsh(mesh, argv[2], 2);
+   
+  // get data after refinement
+  std::vector<std::vector<double>> postAMRData;
+  adapter->getMeshVData("displacement", &postAMRData);
+  std::cout << "postAMRData[1][1] = " << postAMRData[1][1] << std::endl;
+  std::cout << "Size of vector = " << postAMRData.size() << std::endl; 
+
+  // convert refined msh file back to vtk
+  GModel* trgGModel;
+  trgGModel = new GModel("refined"); 
+  trgGModel->readMSH("refined.msh");
+  trgGModel->writeVTK("refined.vtk", false, true); // binary=false, saveall=true
+
+  // write physical quantities to vtk file
+  vtkAnalyzer* trgVTK;
+  trgVTK = new vtkAnalyzer((char*) "refined.vtk");
+  trgVTK->read();
+  trgVTK->report();
+  std::vector<double> postAMRDatas = flatten(postAMRData);
+
+  trgVTK->setPointDataArray("displacement", 3, postAMRDatas);
+  trgVTK->report();
+  trgVTK->write("refined_solution.vtu");
+  trgVTK->writeMSH("refined_solution.msh");
+
+ 
+  //if (localSF) delete localSF;
+  //if (adapter) delete adapter;
+  if (bSF) delete bSF;
+  if (trgGModel) delete trgGModel;
+  if (trgVTK) delete trgVTK;
+  return 0;
+}
+
+
+  /*MAd::PWLSField* pwlSF = new MAd::PWLSField(mesh);
+  pwlSF->setCurrentSize();
+  pwlSF->scale(.5);
+  MAd::MeshAdapter* adapter = new MAd::MeshAdapter(mesh, pwlSF);
+  */
   
 
   /*size_t beg = 0;
@@ -97,24 +230,6 @@ int main(int argc, char* argv[])
 
   std::set<GVertex*, GEntityLessThan>::iterator iter = srcGModel->firstVertex();  
   */
-
-  MAd::pGModel gmodel = 0;
-  MAd::pMesh mesh = MAd::M_new(gmodel);
-  vtkAnalyzer* tmpmesh;
-  if (meshFile.find(".v") != -1 ) 
-  {
-    tmpmesh = new vtkAnalyzer((char*) &(meshFile)[0u]);
-    tmpmesh->read();
-    std::string ofname = "converted.msh";
-    tmpmesh->writeMSH(ofname);
-
-    MAd::M_load(mesh, "converted.msh"); 
-  }
-  else if (meshFile.find(".msh") != -1)
-  {
-    MAd::M_load(mesh, (char*) &(meshFile)[0u]); 
-  }
-
 
   /*/////////////////// LOCAL SIZEFIELD TESTS /////////////////////////    
   MAd::LocalSizeField* localSF = new MAd::LocalSizeField(mesh);
@@ -166,112 +281,3 @@ int main(int argc, char* argv[])
  
   adapter->addSizeField(localSF2);*/ // was used to refine around inclusion, but leave inclusion empty 
   ////////////////////////// END LOCAL SIZEFIELD TESTS ////////////////////
-  
-
-  // find 2d boundary elements
-  MAd::pGEntity bnd = (MAd::pGEntity) MAd::GM_faceByTag(mesh->model, 0);
-  mesh->classify_grid_boundaries(bnd);
-  mesh->classify_unclassified_entities();
-  mesh->destroyStandAloneEntities();
-  
-  /*MAd::PWLSField* pwlSF = new MAd::PWLSField(mesh);
-  pwlSF->setCurrentSize();
-  pwlSF->scale(.5);
-  MAd::MeshAdapter* adapter = new MAd::MeshAdapter(mesh, pwlSF);
-  */
-  
- // writing background mesh and taking care of non tet/tri entities
-  tmpmesh->writeBackgroundMSH("backgroundSF.msh", .1);
-  MAd::BackgroundSF* bSF = new MAd::BackgroundSF("backgroundSF");
-  bSF->loadData("backgroundSF.msh");
-  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-  MAd::MeshAdapter* adapter = new MAd::MeshAdapter(mesh, bSF);
-  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-  std::cout << "Time for adapter construction (ms): " 
-            << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() 
-            << std::endl;
-  // attach data to mesh
-  /* TODO: writing vector data not implemented in madlib
-           instead, splitting vector data into component vectors */  
-  std::vector<std::vector<double>> pntData;
-  int numTuple, numComponent; 
-  tmpmesh->getPointDataArray(0, pntData, numTuple, numComponent);
-
-  /*std::vector<double> pntDatax, pntDatay, pntDataz;
-  for (int i = 0; i < numTuple; ++i)
-  {
-    pntDatax.push_back(pntData[i][0]);
-    pntDatay.push_back(pntData[i][1]);
-    pntDataz.push_back(pntData[i][2]);
-  }
-
-  adapter->registerData("VX", pntDatax);
-  adapter->registerData("VY", pntDatay);
-  */
-
-  adapter->registerVData("V", pntData);
-  // check if data is correctly attached
-  std::vector<std::vector<double>> preAMRDatay;
-  adapter->getMeshVData("V", &preAMRDatay);
-  std::cout << "preAMRDatay[1000][1] = " << preAMRDatay[1000][1] << std::endl;
-  std::cout << "Size of vector = " << preAMRDatay.size() << std::endl; 
-
-  // Output situation before optimization
-  std::cout << "Statistics before optimization: " << std::endl;
-  adapter->printStatistics(std::cout);
-  
-  // Optimize
-  std::cout << "Optimizing the mesh ..." << std::endl;
-  adapter->run();
-  //ma->removeSlivers();
-
-  // Outputs final mesh
-  std::cout << "Statistics after optimization: " << std::endl;
-  adapter->printStatistics(std::cout);
-
-  MAd::M_writeMsh(mesh, argv[2], 2);
-
-   
-  // get data after refinement
-  std::vector<std::vector<double>> postAMRDatay;
-  adapter->getMeshVData("V", &postAMRDatay);
-  std::cout << "postAMRDatay[1000][1] = " << postAMRDatay[1000][1] << std::endl;
-  std::cout << "Size of vector = " << postAMRDatay.size() << std::endl; 
-
-  // convert final Gmsh file back to vtk
-  GModel* trgGModel;
-  trgGModel = new GModel("refined"); 
-  trgGModel->readMSH("refined.msh");
-  trgGModel->writeVTK("refined.vtk", false, true);
-  // binary=false, saveall=true
-
-  // write physical quantities to vtk file
-  vtkAnalyzer* trgVTK;
-  trgVTK = new vtkAnalyzer((char*) "refined.vtk");
-  trgVTK->read();
-  trgVTK->report();
-  std::vector<double> postAMRDatas(numComponent*postAMRDatay.size());
-  for (int i = 0; i < postAMRDatay.size(); ++i)
-  {
-    for (int j = 0; j < numComponent; ++j)
-    {
-      postAMRDatas[i*numComponent+j] = postAMRDatay[i][j];
-    }
-  }
-
-  trgVTK->setPointDataArray("V", 3, postAMRDatas);
-  trgVTK->report();
-  trgVTK->write("refined_solution.vtu");
-
-
- 
-  //if (localSF) delete localSF;
-  //if (adapter) delete adapter;
-  if (bSF) delete bSF;
-  if (trgGModel) delete trgGModel;
-  if (trgVTK) delete trgVTK;
-  return 0;
-}
-
-
-
