eb8722552b21eae3845716b4a0f2644be8bf46e2
snatesh <sachin.natesh@illinoisrocstar.com>
2017-10-26 19:09:43 -0500

    added method for refinement by value

--- src/meshPhys.C
+++ src/meshPhys.C
@@ -45,21 +45,97 @@ std::vector<double> meshPhys::ComputeGradAtCell(int cell, int array)
   }
 }
 
-// computes value of point data at a cell using shape interpolation functions
-std::vector<double> meshPhys::ComputeValAtCell(int cell, int array){}
+// computes value of point data at a cell center using shape interpolation functions
+std::vector<double> meshPhys::ComputeValAtCell(int cell, int array)
+{
+  if (!pointData)
+    pointData = dataSet->GetPointData();
+  if (pointData)
+  {
+    vtkIdList* point_ids = dataSet->GetCell(cell)->GetPointIds();
+    int numPointsInCell = point_ids->GetNumberOfIds();
+    int dim = pntData[array].getNumComponent();
+    
+    // evaluate center of cell in cartesian coords
+    std::vector<double*> cell_coords = getCellCoords(cell,numPointsInCell);
+    double x, y, z;
+    x = y = z = 0;
+    for (int i = 0; i < numPointsInCell; ++i)
+    {
+      x+= cell_coords[i][0];
+      y+= cell_coords[i][1];
+      z+= cell_coords[i][2];
+      delete cell_coords[i];   
+    }
+    
+    double center[3];
+    center[0] = x/numPointsInCell;
+    center[1] = y/numPointsInCell;
+    center[2] = z/numPointsInCell;
+    
+    // evaluate parametric position of center and compute interpolation weights
+    int subId;
+    double minDist2; // not used
+    double pcoords[3];
+    double weights[numPointsInCell];
+    dataSet->GetCell(cell)->EvaluatePosition(center, NULL, subId, pcoords, minDist2, weights);    
 
+    // compute value of data at center of cell
+    std::vector<double> values(dim); 
+    for (int i = 0; i < dim; ++i) // loop over values per vertex
+    {
+      double val=0;
+      for (int j = 0; j < numPointsInCell; ++j) // loop over vertices in cell
+      {
+        int id = (int) point_ids->GetId(j);
+        val += pntData[array](id,i)*weights[j];
+      }
+      values[i] = val;
+    }
+    return values;
+  }
+}
+
+// compute L2 norm of gradient of point data at each cell 
 std::vector<double> meshPhys::ComputeL2GradAtAllCells(int array)
 {
   std::vector<double> result;
   result.resize(numberOfCells);
   for (int i = 0; i < numberOfCells; ++i)
   {
-    result[i] = L2_Norm(meshPhys::ComputeGradAtCell(i, array)); 
+    result[i] = L2_Norm(ComputeGradAtCell(i, array)); 
   }
 
   return result;
 }
 
+// compute value of point data at center of each cell
+std::vector<double> meshPhys::ComputeValAtAllCells(int array)
+{
+  int dim = getDimArray(array);
+  std::vector<double> result(numberOfCells*dim);
+  for (int i = 0; i < numberOfCells; ++i)
+  {
+    std::vector<double> values = ComputeValAtCell(i, array);
+    for (int j = 0; j < dim; ++j)
+      result[i*dim + j] = values[j];
+  }
+  return result;
+}
+
+// compute L2 norm of value of point data at center of each cell
+std::vector<double> meshPhys::ComputeL2ValAtAllCells(int array)
+{
+  int dim = getDimArray(array);
+  if (dim == 1)
+    return ComputeValAtAllCells(array);
+  std::vector<double> result(numberOfCells); 
+  for (int i = 0; i < numberOfCells; ++i)
+    result[i] = L2_Norm(ComputeValAtCell(i, array));
+  return result;
+}
+
+// get diameter of circumsphere of each cell
 std::vector<double> meshPhys::GetCellLengths()
 {
   std::vector<double> result;
@@ -71,9 +147,8 @@ std::vector<double> meshPhys::GetCellLengths()
   return result;
 }
 
-
-
-// writes a background mesh with sizes as point data
+// writes a background mesh with sizes defined by 
+// point data interpolated to cell center
 void meshPhys::writeBackgroundMSH(string filename, std::vector<double> sizes)
 {
   std::ofstream outputStream(filename.c_str());
