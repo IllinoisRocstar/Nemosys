b73ec52f3dfd82343c9dfcaf25f56ab7883428c7
snatesh <sachin.natesh@illinoisrocstar.com>
2018-01-29 22:06:53 -0600

    integrated gtest and implemented Cubature class

--- utils/nemTest.C
+++ utils/nemTest.C
@@ -1,11 +1,17 @@
-#include <meshBase.H>
-
+#include <Cubature.H>
 int main(int argc, char* argv[])
 {
   
   meshBase* mesh = meshBase::Create("refined1_transfer.vtu");
-  mesh->getIntegrationPointsAtCell(50);
+  //mesh->getIntegrationPointsAtCell(50);
+  GaussCubature* cuby = new  GaussCubature(mesh);
+  std::vector<int> tmp = {0,2,3,7};
+  cuby->interpolateToGaussPoints(tmp);
+  meshBase* mesh1 = meshBase::Create("refined1_transferGaussPoints.vtp");
+  mesh1->write("tmptmptmp.vtp");
+  delete cuby;
   delete mesh;
+  delete mesh1;
   return 0;
 
 }
