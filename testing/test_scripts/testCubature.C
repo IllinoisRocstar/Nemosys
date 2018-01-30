b1be9c7837f8bd6dd23c696f30e2e74ebeb5b0ed
snatesh <sachin.natesh@illinoisrocstar.com>
2018-01-30 12:59:27 -0600

    modified gitignore, added gtest

--- testing/test_scripts/testCubature.C
+++ testing/test_scripts/testCubature.C
@@ -1,91 +1,6 @@
 #include <Cubature.H>
 #include <gtest.h>
 
-int diffVTU(meshBase* mesh1, meshBase* mesh2)
-{
-  double tol = 1e-6;
-
-  if (mesh1->getNumberOfPoints() != mesh2->getNumberOfPoints() ||
-      mesh1->getNumberOfCells() != mesh2->getNumberOfCells())
-  {
-    std::cerr << "Meshes don't have the same number of points or cells" << std::endl;
-    exit(1);
-  }
-
-  for (int i = 0; i < mesh1->getNumberOfPoints(); ++i)
-  {
-    std::vector<double> coord1 = mesh1->getPoint(i);
-    std::vector<double> coord2 = mesh2->getPoint(i);
-    for (int j = 0; j < 3; ++j)
-    {
-      if (std::fabs(coord1[j]-coord2[j]) > tol)
-      {
-        std::cerr << "Meshes differ in point coordinates" << std::endl;
-        exit(1);
-      }
-    } 
-  }
-
-  for (int i = 0; i < mesh1->getNumberOfCells(); ++i)
-  {
-    std::vector<std::vector<double>> cell1 = mesh1->getCellVec(i);
-    std::vector<std::vector<double>> cell2 = mesh2->getCellVec(i);
-    if (cell1.size() != cell2.size())
-    {
-      std::cerr << "Meshes differ in cells" << std::endl;
-      exit(1); 
-    }
-    for (int j = 0; j < cell1.size(); ++j)
-    {
-      for (int k = 0; k < 3; ++k)
-      {
-        if (std::fabs(cell1[j][k] - cell2[j][k]) > tol)
-        {
-          std::cerr << "Meshes differ in cells" << std::endl;
-          exit(1); 
-        }
-      }
-    }   
-  }
-
-  vtkSmartPointer<vtkPointData> pd1 = vtkSmartPointer<vtkPointData>::New();
-  pd1 = mesh1->getDataSet()->GetPointData();
-  vtkSmartPointer<vtkPointData> pd2 = vtkSmartPointer<vtkPointData>::New();
-  pd2 = mesh2->getDataSet()->GetPointData(); 
-  int numArr1 = pd1->GetNumberOfArrays(); 
-  int numArr2 = pd2->GetNumberOfArrays(); 
- 
-  if (numArr1 != numArr2)
-  {
-    std::cerr << "Meshes have different numbers of point data" << std::endl;
-    exit(1);
-  }
-
-  for (int i = 0; i < numArr1; ++i)
-  {
-    vtkDataArray* da1 = pd1->GetArray(i);
-    vtkDataArray* da2 = pd2->GetArray(i);
-    int numComponent = da1->GetNumberOfComponents();
-    for (int j = 0; j < mesh1->getNumberOfPoints(); ++j)
-    {
-      double comps1[numComponent];
-      double comps2[numComponent];
-      da1->GetTuple(j, comps1);
-      da2->GetTuple(j, comps2);
-      for (int k = 0; k < numComponent; ++k)
-      {
-        if (std::fabs(comps1[k] - comps2[k]) > tol)
-        {
-          std::cerr << "Meshes differ in point data values" << std::endl;
-          exit(1);
-        }
-      }
-    }
-  }
-  std::cerr << "Meshes are the same" << std::endl;
-	exit(0);
-}
-
 const char* nodeMesh;
 const char* refGauss;
 // The fixture for testing class orthoPoly. From google test primer.
@@ -133,14 +48,15 @@ class CubatureTest : public ::testing::Test
     const std::vector<int> arrayIDs = {0,2,3,7}; 
 };
 
+
+// tests construction, writeout, loading and interpolation
 TEST_F(CubatureTest, InterpolateToGauss)
 {
   cuby->interpolateToGaussPoints(arrayIDs);   
   cuby->writeGaussMesh();
   meshBase* gaussMesh = meshBase::Create("gaussTest.vtp");
   meshBase* refGaussMesh = meshBase::Create(refGauss);
-  EXPECT_EXIT(diffVTU(gaussMesh,refGaussMesh), ::testing::ExitedWithCode(0),
-              "Meshes are the same"); 
+  EXPECT_EQ(0,diffVTU(gaussMesh,refGaussMesh));
 	if (gaussMesh) delete gaussMesh;
 	if (refGaussMesh) delete refGaussMesh;
 }
