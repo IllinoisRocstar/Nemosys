33204b8e9d30a81029f01ea3314bb0c8f4728634
snatesh <sachin.natesh@illinoisrocstar.com>
2018-01-30 21:21:35 -0600

    forgot

--- utils/nemTest.C
+++ utils/nemTest.C
@@ -5,14 +5,14 @@ int main(int argc, char* argv[])
   meshBase* mesh = meshBase::Create("refined1_transfer.vtu");
   //mesh->getIntegrationPointsAtCell(50);
   GaussCubature* cuby = new  GaussCubature(mesh);
-  std::vector<int> tmp = {0,2,3,7};
-  cuby->interpolateToGaussPoints(tmp);
-	cuby->writeGaussMesh();
-  meshBase* mesh1 = meshBase::Create("refined1_transferGaussPoints.vtp");
-  mesh1->write("tmptmptmp.vtp");
+  //std::vector<int> tmp = {0,2,3,7};
+  //cuby->interpolateToGaussPoints(tmp);
+	cuby->constructGaussMesh({0,2,3,7});
+  //meshBase* mesh1 = meshBase::Create("refined1_transferGaussPoints.vtp");
+  //mesh1->write("tmptmptmp.vtp");
   delete cuby;
   delete mesh;
-  delete mesh1;
+  //delete mesh1;
   return 0;
 
 }
