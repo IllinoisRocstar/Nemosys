f45717352a9063029097e40e352d14a62408ac15
snatesh <sachin.natesh@illinoisrocstar.com>
2018-02-02 16:27:52 -0600

    further development on patchRecovery

--- src/PatchRecovery/patchRecovery.C
+++ src/PatchRecovery/patchRecovery.C
@@ -1,32 +1,82 @@
 #include <patchRecovery.H>
 
-PatchRecovery::PatchRecovery(meshBase* nodeMesh, const std::vector<int>& arrayIDs)
+
+PatchRecovery::PatchRecovery(meshBase* nodeMesh, int _order, const std::vector<int>& arrayIDs)
+	: order(_order)
 {
   cubature = GaussCubature::CreateUnique(nodeMesh, arrayIDs);
     
 }
 
-void PatchRecovery::recoverNodalSolution(const std::vector<int>& arrayIDs)
+//TODO: stop being an idiot and change the type of pntDataPairVec to hold eigen vec type for data
+
+// extract coordinates and data from cell by pushing into coords and data
+void PatchRecovery::extractAxes(pntDataPairVec& pntsAndData, 
+																std::vector<std::vector<double>>& coords)
 {
-  std::cout << "WARNING: mesh is assumed to be properly numbered" << std::endl;
-  meshBase* nodeMesh = cubature->getNodeMesh();
-  int numPoints = nodeMesh->getNumberOfPoints();
-  vtkSmartPointer<vtkIdList> patchCellIDs = vtkSmartPointer<vtkIdList>::New();
-  for (int i = 0; i < numPoints; ++i)
-  {
-    
-    // get ids of cells in patch of node
-    nodeMesh->getDataSet()->GetPointCells(i,patchCellIDs);
-    for (int j = 0; j < patchCellIDs->GetNumberOfIds(); ++j)
-    {
-      pntDataPairVec pntsAndData = cubature->getGaussPointsAndDataAtCell(patchCellIDs->GetId(j));
-      // TODO: efficient way to extract x,y,z in std::vec and data into VectorXd
-      //       note move semantics can be used because we don't care about 
-      //       pntDataPairVec once we've got the info   
 
-    } 
+	// number of components for each data being considered
+	// {x,y,z} -> {{d1_1, ... ,d1_m} , ... , {dk_1, ... , dk_m}} ; i_m != k_m in general
+	std::vector<int> numComponents = cubature->getNumComponents();			
 
-
-  } 
-
+	for (int i = 0; i < pntsAndData.size(); ++i)
+	{
+		coords.push_back(std::move(pntsAndData[i].first));
+	}
 }
+													
+// pair type for coordinate and data there
+//typedef std::pair<std::vector<double>,std::vector<std::vector<double>>> pntDataPair;
+
+// holds gauss points and data at these points as pairs
+//typedef std::vector<pntDataPair> pntDataPairVec;
+
+
+//void PatchRecovery::recoverNodalSolution(const std::vector<int>& arrayIDs)
+//{
+//  std::cout << "WARNING: mesh is assumed to be properly numbered" << std::endl;
+//  meshBase* nodeMesh = cubature->getNodeMesh();
+//  int numPoints = nodeMesh->getNumberOfPoints();
+//  vtkSmartPointer<vtkIdList> patchCellIDs = vtkSmartPointer<vtkIdList>::New();
+//  vtkQuadratureSchemeDefinition** dict = cubature->getDict();
+//	
+//	std::vector<int> numComponents = cubature->getNumComponents();
+//
+//	for (int i = 0; i < numPoints; ++i)
+//  {
+//    
+//		std::vector<std::vector<double>> coords;
+//		std::vector<std::vector<VectorXd>> data;
+//
+//    // get ids of cells in patch of node
+//    nodeMesh->getDataSet()->GetPointCells(i,patchCellIDs);
+//		int numGaussPoints = 0;
+//		for (int j = 0; j < patchCellIDs->GetNumberOfIds(); ++j)
+//		{
+//			int cellType = nodeMesh->getDataSet()->GetCell(patchCellIDs->GetId(j))->GetCellType();
+//			numGaussPoints += dict[cellType]->GetNUmberOfQuadraturePoints();
+//		}		
+//
+//    for (int j = 0; j < patchCellIDs->GetNumberOfIds(); ++j)
+//    {
+//		
+//      pntDataPairVec pntsAndData = cubature->getGaussPointsAndDataAtCell(patchCellIDs->GetId(j));
+//			extractAxesAndData(pntsAndData, coords, data);
+//    }
+//
+//		//TODO: SOMETHING LIKE THIS
+//		// construct orthopoly from coords
+//		std::unique_ptr<orthoPoly3D> patchPolyApprox(new orthoPoly3D(order, coords));
+//		for (int k = 0; k < numComponents.size(); ++k)
+//		{	
+//			for (int l = 0; l < numComponents[k].size(); ++k)
+//			{
+//				patchPolyApprox->computeA(data[k][l]);	
+//			
+//			}
+//	
+//		}
+//
+//  } 
+//
+//}
