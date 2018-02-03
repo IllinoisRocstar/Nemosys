fd99825fafa15862ec208723982ee131ead45b73
snatesh <sachin.natesh@illinoisrocstar.com>
2018-02-02 21:45:39 -0600

    changed pntDataPairVec type to hold eigen VectorXd

--- testing/test_scripts/testCubature.C
+++ testing/test_scripts/testCubature.C
@@ -83,11 +83,16 @@ TEST_F(CubatureTest, InterpolateToGauss)
   //  double jacobi = cuby->computeScaledJacobian(genCell,VTK_TETRA);
   //  std::cout << jacobi << std::endl;
   //}
-  //{
-  //  pntDataPairVec shit = cuby->getGaussPointsAndDataAtCell(i);
-  //  for (int k = 0; k < shit.size(); ++k)
-  //    printVec(shit[k].second[3]); 
-  //}
+  {
+    pntDataPairVec shit = cuby->getGaussPointsAndDataAtCell(300);
+    for (int k = 0; k < shit.size(); ++k)
+		{
+			for (int j = 0; j < 3; ++j)
+				std::cout << shit[k].second[3](j) << " ";
+      //printVec(shit[k].second[3]); 
+  		std::cout << std::endl;
+		}
+	}
 }
 
 int main(int argc, char** argv) {
