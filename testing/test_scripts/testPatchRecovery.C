c2b6830c3c0c16f80f02edf4b1a645c3f8db2778
snatesh <sachin.natesh@illinoisrocstar.com>
2018-02-26 22:38:33 -0600

    completed patch recovery

--- testing/test_scripts/testPatchRecovery.C
+++ testing/test_scripts/testPatchRecovery.C
@@ -2,24 +2,26 @@
 #include <gtest.h>
 
 const char* nodeMesh;
-
+const char* recoveredMesh;
 
 TEST(PatchRecoveryConstructor, ConstructWithoutArray)
 {
   // load reference node mesh
   std::unique_ptr<meshBase> mesh = meshBase::CreateUnique(nodeMesh);
-  const std::vector<int> arrayIDs = {0,1,2,3,4,5};
+  const std::vector<int> arrayIDs = {0,1,2,3,4,5,7};
   int order = 1;
   std::unique_ptr<PatchRecovery> recoverObj
     = std::unique_ptr<PatchRecovery> (new PatchRecovery(mesh.get(),order,arrayIDs));
   recoverObj->recoverNodalSolution();
-  mesh->write("test.vtu");
+  std::unique_ptr<meshBase> refMesh = meshBase::CreateUnique(recoveredMesh);
+  EXPECT_EQ(0,diffMesh(mesh.get(),refMesh.get()));
 }
 
  
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  assert(argc == 2);
+  assert(argc == 3);
   nodeMesh = argv[1];
+  recoveredMesh = argv[2];
   return RUN_ALL_TESTS();
 }
