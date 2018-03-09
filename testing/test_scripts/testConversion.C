2e2f816d6b9cbe3cf7a3f7fdf5b8dd5a03ae9bcc
snatesh <sachin.natesh@illinoisrocstar.com>
2018-03-09 17:03:44 -0600

    testing for legacy conversion robustness

--- testing/test_scripts/testConversion.C
+++ testing/test_scripts/testConversion.C
@@ -5,6 +5,10 @@ const char* mshName;
 const char* volName;
 const char* refMshVTUName;
 const char* refVolVTUName;
+const char* legacyVTK1;
+const char* legacyVTK2;
+const char* legacyVTK1_ref;
+const char* legacyVTK2_ref;
 
 TEST(Conversion, ConvertGmshToVTK)
 {
@@ -20,14 +24,33 @@ TEST(Conversion, ConvertVolToVTK)
   EXPECT_EQ(0,diffMesh(mesh.get(),refMesh.get()));
 }
 
+TEST(Conversion, ConvertLegacyVTKToVTU)
+{
+	std::unique_ptr<meshBase> mesh = meshBase::CreateUnique(legacyVTK1);
+	std::unique_ptr<meshBase> mesh_ref = meshBase::CreateUnique(legacyVTK1_ref);
+	std::unique_ptr<meshBase> mesh1 = meshBase::CreateUnique(legacyVTK2);
+	std::unique_ptr<meshBase> mesh1_ref = meshBase::CreateUnique(legacyVTK2_ref);
+	std::cout << mesh->getNumberOfCells() << std::endl;
+	std::cout << mesh_ref->getNumberOfCells() << std::endl;
+	std::cout << mesh1->getNumberOfCells() << std::endl;
+	std::cout << mesh1_ref->getNumberOfCells() << std::endl;
+	//mesh1->write("legacyVTK2_ref.vtu");
+	//mesh->write("legacyVTK1_ref.vtu");
+	EXPECT_EQ(0, diffMesh(mesh.get(), mesh_ref.get()));
+	EXPECT_EQ(0, diffMesh(mesh1.get(), mesh1_ref.get()));
+}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  assert(argc == 5);
+  assert(argc == 9);
   refMshVTUName = argv[1];
   mshName = argv[2];
   refVolVTUName = argv[3];
   volName = argv[4];
+	legacyVTK1 = argv[5];
+	legacyVTK2 = argv[6];
+	legacyVTK1_ref = argv[7];
+	legacyVTK2_ref = argv[8];
   return RUN_ALL_TESTS();
 }
 
