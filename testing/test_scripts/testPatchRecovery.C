5b20e2fd02bd70a5a8138150425f4a90c3c52056
snatesh <sachin.natesh@illinoisrocstar.com>
2018-04-03 15:08:54 -0500

    support for degenerate vtk

--- testing/test_scripts/testPatchRecovery.C
+++ testing/test_scripts/testPatchRecovery.C
@@ -18,25 +18,25 @@ TEST(PatchRecoveryConstructor, ConstructWithArray)
   mesh->write("errorTest.vtu");
 }
 
-TEST(PatchRecoveryTensorProdBasis, RecoverNodalSol)
-{
-  std::unique_ptr<meshBase> mesh = meshBase::CreateUnique(hexmesh);
-  const std::vector<int> arrayIDs = {0,1};
-  int order = 1;
-  std::unique_ptr<PatchRecovery> recoverObj
-    = std::unique_ptr<PatchRecovery> (new PatchRecovery(mesh.get(),order,arrayIDs));
-  Timer T;
-  T.start();
-  recoverObj->recoverNodalSolution(0);
-  T.stop();
-  std::cout << "time for regpoly (ms): " << T.elapsed() << std::endl;
-  mesh->write("polyApproxRecovery.vtu");
-  T.start();
-  recoverObj->recoverNodalSolution(1);
-  T.stop();
-  std::cout << "time for orthopoly(ms): " << T.elapsed() << std::endl;
-  mesh->write("orthoPolyApproxRecovery.vtu");
-}
+//TEST(PatchRecoveryTensorProdBasis, RecoverNodalSol)
+//{
+//  std::unique_ptr<meshBase> mesh = meshBase::CreateUnique(hexmesh);
+//  const std::vector<int> arrayIDs = {0,1};
+//  int order = 1;
+//  std::unique_ptr<PatchRecovery> recoverObj
+//    = std::unique_ptr<PatchRecovery> (new PatchRecovery(mesh.get(),order,arrayIDs));
+//  Timer T;
+//  T.start();
+//  recoverObj->recoverNodalSolution(0);
+//  T.stop();
+//  std::cout << "time for regpoly (ms): " << T.elapsed() << std::endl;
+//  mesh->write("polyApproxRecovery.vtu");
+//  T.start();
+//  recoverObj->recoverNodalSolution(1);
+//  T.stop();
+//  std::cout << "time for orthopoly(ms): " << T.elapsed() << std::endl;
+//  mesh->write("orthoPolyApproxRecovery.vtu");
+//}
  
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
