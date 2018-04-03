d4a3d88c9580f0c8a56888f0fd2624270c6985b3
snatesh <sachin.natesh@illinoisrocstar.com>
2018-04-03 15:08:54 -0500

    support for degenerate vtk

--- src/StlToVtk.C
+++ src/StlToVtk.C
@@ -1,20 +1,5 @@
 #include <StlToVtk.H>
 
-template<typename A, typename B>
-std::pair<B,A> flip_pair(const std::pair<A,B> &p)
-{
-    return std::pair<B,A>(p.second, p.first);
-}
-
-template<typename A, typename B>
-std::multimap<B,A> flip_map(const std::map<A,B> &src)
-{
-    std::multimap<B,A> dst;
-    std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()), 
-                   flip_pair<A,B>);
-    return dst;
-}
-
 void exportStlToVtk(std::string ifname)
 {
   std::ifstream stl(ifname.c_str());
