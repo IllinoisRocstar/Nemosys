906821e301ef3b83378d8a721c82d3cc9ec0fce3
snatesh <sachin.natesh@illinoisrocstar.com>
2018-05-25 23:06:16 -0500

    small edits to remesh utility

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,12 @@
 
 # This script will build Nemosys and all of the necessary dependencies
 # given a tarball of projects with gmsh, madlib, and cgns.
-# Usage: ./build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR
+# Usage: 
+#	- without simmetrix support
+#		./build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR
+#	- with simmetrix support
+#		./ build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR PATH_TO_HDF5
+
 
 set -x
 
@@ -60,12 +65,14 @@ make -j8
 make install
 ls Mesh/MeshDataBase.h Mesh/MeshDataBaseInterface.h Mesh/MeshDataBaseIterators.h Mesh/MeshDataBaseAttachable.h Mesh/MeshDataBaseMiniMesh.h Mesh/MshTags.h Mesh/MeshDataBaseIO.h Mesh/CheckOrientation.h Common/MAdMessage.h Common/MAdSingleton.h Geo/GmshEntities.h Geo/Physical.h Adapt/utils/NodalDataManager.h | xargs -I {} cp {} $NEMOSYS_DEPS_INSTALL_PATH/madlib/include/MAdLib
 
+HDF5_DIR=$4
+
 # build cgns
 cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
 tar zxf cgns.tar.gz
 cd CGNS
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/cgns ..
+CMAKE_PREFIX_PATH=$HDF5_DIR cmake -DCGNS_ENABLE_HDF5=ON -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/cgns ..
 make -j8
 make install
