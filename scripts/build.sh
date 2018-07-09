88b97472d347596d25ce6433e0b35e6a8cc864ee
snatesh <sachin.natesh@illinoisrocstar.com>
2018-07-08 19:03:45 -0500

    adding vtk to tpls, build sys and script changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@
 #	- without simmetrix support
 #		./build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR
 #	- with simmetrix support
-#		./ build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR PATH_TO_HDF5
+#		./ build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR PATH_TO_HDF5_INC PATH_TO_HDF5_LIB
 
 
 set -x
@@ -16,6 +16,9 @@ NEMOSYS_PROJECT_PATH=$1
 #NEMOSYS_DEPS_INSTALL_PATH=$NEMOSYS_PROJECT_PATH/install
 NEMOSYS_DEPS_INSTALL_PATH=$3
 NEMOSYS_TARBALL_PATH=$2
+
+# get number of threads for building
+num_threads=$(nproc)
 
 # clean
 rm -rf $NEMOSYS_DEPS_BUILD_DIR
@@ -28,21 +31,13 @@ cp $NEMOSYS_TARBALL_PATH .
 tar zxf nemosys_tpls.tar.gz
 
 # build netgen
-#cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
-#tar xzf netgen-5.3.1.tar.gz
-#cd netgen-5.3.1
-#./configure --prefix=$NEMOSYS_DEPS_INSTALL_PATH/netgen/opt/netgen --exec-prefix=$NEMOSYS_DEPS_INSTALL_PATH/netgen/opt/netgen --with-tcl=/usr/lib/tcl8.5/ --with-tk=/usr/lib/tk8.5/ --with-togl=/usr/lib --enable-shared --enable-nglib
-#make
-#make install
-
-# build netgen
 cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
 tar xzf netgen-mesher-git.tar.gz
 cd netgen-mesher-git
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/netgen -DUSE_GUI=OFF ..
-make
+make -j${num_threads}
 make install
 
 # build gmsh
@@ -52,7 +47,7 @@ cd gmsh-2.15.0-source
 mkdir lib
 cd lib
 cmake -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/gmsh -DDEFAULT=0 -DENABLE_BUILD_LIB=1 -DENABLE_BUILD_SHARED=1 ..
-make lib shared install/fast -j8
+make lib shared install/fast -j${num_threads}
 cp ../Mesh/meshPartitionObjects.h $NEMOSYS_DEPS_INSTALL_PATH/gmsh/include/gmsh/
 cp ../Mesh/Generator.h $NEMOSYS_DEPS_INSTALL_PATH/gmsh/include/gmsh/
 
@@ -61,18 +56,29 @@ cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
 tar zxf madlib-1.3.0.tar.gz
 cd madlib-1.3.0/
 ./configure --prefix=$NEMOSYS_DEPS_INSTALL_PATH/madlib --enable-moveIt --enable-benchmarks --enable-ann --enable-gmsh --with-gmsh-prefix=$NEMOSYS_DEPS_INSTALL_PATH/gmsh
-make -j8
+make -j${num_threads}
 make install
 ls Mesh/MeshDataBase.h Mesh/MeshDataBaseInterface.h Mesh/MeshDataBaseIterators.h Mesh/MeshDataBaseAttachable.h Mesh/MeshDataBaseMiniMesh.h Mesh/MshTags.h Mesh/MeshDataBaseIO.h Mesh/CheckOrientation.h Common/MAdMessage.h Common/MAdSingleton.h Geo/GmshEntities.h Geo/Physical.h Adapt/utils/NodalDataManager.h | xargs -I {} cp {} $NEMOSYS_DEPS_INSTALL_PATH/madlib/include/MAdLib
 
-HDF5_DIR=$4
+HDF5_INC=$4
+HDF5_LIB=$5
 
 # build cgns
 cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
-tar zxf cgns.tar.gz
-cd CGNS
+tar zxf cgnslib_3.2.1.tar.gz
+cd cgnslib_3.2.1
 mkdir build
 cd build
-CMAKE_PREFIX_PATH=$HDF5_DIR cmake -DCGNS_ENABLE_HDF5=ON -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/cgns ..
-make -j8
+cmake -DCGNS_ENABLE_HDF5=ON -DCGNS_BUILD_CGNSTOOLS=ON -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/cgns -DHDF5_INCLUDE_PATH=${HDF5_INC} -DHDF5_LIBRARY=${HDF5_LIB} ..
+make -j${num_threads}
+make install
+
+# build vtk
+cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
+tar xzf VTK-8.1.1.tar.gz
+cd VTK-8.1.1
+mkdir build
+cd build
+cmake -DVTK_Group_MPI=ON -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/vtk ..
+make -j${num_threads}
 make install
