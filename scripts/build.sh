1b53691ae7dca25082b6fd682d8cc1b3e30f5620
snatesh <sachin.natesh@illinoisrocstar.com>
2018-07-08 23:39:42 -0500

    more build sys stuff, adding hdf5 to tpls and linking to cgns

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # This script will build Nemosys and all of the necessary dependencies
-# given a tarball of projects with gmsh, madlib, and cgns.
+# given a tarball of projects with gmsh, madlib, hdf5, cgns, netgen and vtk.
 # Usage: 
 #	- without simmetrix support
 #		./build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR
-#	- with simmetrix support
-#		./ build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR PATH_TO_HDF5_INC PATH_TO_HDF5_LIB
 
 
 set -x
@@ -60,8 +58,13 @@ make -j${num_threads}
 make install
 ls Mesh/MeshDataBase.h Mesh/MeshDataBaseInterface.h Mesh/MeshDataBaseIterators.h Mesh/MeshDataBaseAttachable.h Mesh/MeshDataBaseMiniMesh.h Mesh/MshTags.h Mesh/MeshDataBaseIO.h Mesh/CheckOrientation.h Common/MAdMessage.h Common/MAdSingleton.h Geo/GmshEntities.h Geo/Physical.h Adapt/utils/NodalDataManager.h | xargs -I {} cp {} $NEMOSYS_DEPS_INSTALL_PATH/madlib/include/MAdLib
 
-HDF5_INC=$4
-HDF5_LIB=$5
+# build hdf5
+cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
+tar zxf hdf5-1.8.21.tar.gz
+cd hdf5-1.8.21
+./configure --prefix=$NEMOSYS_DEPS_INSTALL_PATH/hdf5
+make -j${num_threads}
+make install
 
 # build cgns
 cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
@@ -69,7 +72,7 @@ tar zxf cgnslib_3.2.1.tar.gz
 cd cgnslib_3.2.1
 mkdir build
 cd build
-cmake -DCGNS_ENABLE_HDF5=ON -DCGNS_BUILD_CGNSTOOLS=ON -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/cgns -DHDF5_INCLUDE_PATH=${HDF5_INC} -DHDF5_LIBRARY=${HDF5_LIB} ..
+cmake -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/cgns -DCGNS_BUILD_SHARED=ON -DCGNS_BUILD_CGNSTOOLS=ON -DCGNS_ENABLE_HDF5=ON -DHDF5_INCLUDE_PATH=$NEMOSYS_DEPS_INSTALL_PATH/hdf5/include -DHDF5_LIBRARY=$NEMOSYS_DEPS_INSTALL_PATH/hdf5/lib/libhdf5.so ..
 make -j${num_threads}
 make install
 
@@ -79,6 +82,8 @@ tar xzf VTK-8.1.1.tar.gz
 cd VTK-8.1.1
 mkdir build
 cd build
+export CC=mpicc
+export CXX=mpicxx
 cmake -DVTK_Group_MPI=ON -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/vtk ..
 make -j${num_threads}
 make install
