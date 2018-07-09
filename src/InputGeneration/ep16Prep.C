8b93f3b42d7a9ae3909db709e659676e69e448f6
Masoud Safdari <msafdari@illinoisrocstar.com>
2018-07-09 08:35:32 -0500

    Continuing implementation of input file writer

--- src/InputGeneration/ep16Prep.C
+++ src/InputGeneration/ep16Prep.C
@@ -3,6 +3,7 @@
 #include "AuxiliaryFunctions.H"
 #include "inputGen.H"
 #include "ep16Prep.H"
+#include "exoMesh.H"
  
 
 ep16Prep* ep16Prep::readJSON(std::string ifname)
@@ -96,6 +97,11 @@ void ep16Prep::process()
     wrtCmnt("Generated on " + getTimeStr() + " by NEMoSys");
     wrtCmnt("Short Form Description Card for ExodusII/CUBIT Data");
 
+    // reading exodus databse
+    std::string exo_fname = _jstrm["Mesh"]["File"].as<std::string>();
+    EXOMesh::exoMesh* ex = new EXOMesh::exoMesh(exo_fname);
+    ex->read();
+
     // begin processing based on order specified
     std::vector<std::string> ord = getOrder();
     for (auto oi=ord.begin(); oi!=ord.end(); oi++)
@@ -105,6 +111,7 @@ void ep16Prep::process()
         std::cout << tsk << " -- " << _tsk << std::endl;
         if (!tsk.compare("prep"))
             wrtPre(_tsk);
+
     }
 }
 
@@ -118,24 +125,48 @@ void ep16Prep::wrtCmnt(std::string cmnt)
 
 void ep16Prep::wrtPre(std::string tsk)
 {
-   std::stringstream _tcmnt;
-   std::stringstream _tstr;
-   if (!tsk.compare("case"))
-   {
-       _tstr.clear();
-       _tcmnt.clear();
-       int _ctype = _jstrm["Case"]["Type"].as<int>();
-       int _cid =  _jstrm["Case"]["Id"].as<int>();
-       std::string _des = " " + _jstrm["Case"]["Description"].as<std::string>();
-       _tcmnt << "CASE DESCRIPTION";
-       _tstr << std::setw(5) << _ctype 
-             << std::setw(5) << _cid
-             << std::setw(70) << std::left << _des;
-   }
-   if (!_tcmnt.str().empty())
-       wrtCmnt(_tcmnt.str());
-   if (!_tstr.str().empty())
-       _write(_tstr.str());
+    std::stringstream _tcmnt;
+    std::stringstream _tstr;
+    if (!tsk.compare("case"))
+    {
+        _tstr.clear();
+        _tcmnt.clear();
+        int _ctype = _jstrm["Case"]["Type"].as<int>();
+        int _cid =  _jstrm["Case"]["Id"].as<int>();
+        std::string _des = " " + _jstrm["Case"]["Description"].as<std::string>();
+        _tcmnt << "CASE DESCRIPTION";
+        _tstr << std::setw(5) << _ctype 
+              << std::setw(5) << _cid
+              << std::setw(70) << std::left << _des;
+    }
+    else if (!tsk.compare("run"))
+    {
+        _tstr.clear();
+        _tcmnt.clear();
+        int _rmde, _unt, _pat, _dplt, _ssld;
+        double _tmx, _cpmx;
+        _pat = 3;
+        _dplt = 3;
+        _cpmx = 0.0;
+        _rmde = _jstrm["Run"]["Mode"].as<int>();
+        _unt = _jstrm["Run"]["Unit"].as<int>();
+        _tmx = _jstrm["Run"]["Tmax"].as<double>();
+        _tcmnt << "Run Card";
+        _tstr.precision(4);
+        _tstr << std::setw(5) << _rmde
+              << std::setw(5) << _unt
+              << std::setw(5) << _pat
+              << std::setw(5) << _dplt
+              << std::setw(10) << std::scientific << _tmx
+              << std::setw(10) << _cpmx
+              << std::setw(5) << _ssld;
+    }
+
+    // write to stream
+    if (!_tcmnt.str().empty())
+        wrtCmnt(_tcmnt.str());
+    if (!_tstr.str().empty())
+        _write(_tstr.str());
 }
 
 
