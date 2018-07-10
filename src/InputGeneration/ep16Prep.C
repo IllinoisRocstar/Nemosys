2261166368df48450edbc121681bb48f8529226d
Masoud Safdari <msafdari@illinoisrocstar.com>
2018-07-10 12:49:54 -0500

    finished ep16 input processor, needs testing

--- src/InputGeneration/ep16Prep.C
+++ src/InputGeneration/ep16Prep.C
@@ -84,7 +84,7 @@ void ep16Prep::readJSON()
         "mesh.nodeset.projectile",
         "mesh.nodeset.target",
         "mesh.elementset.projectile",
-        "mesh.elrmrnyset.target",
+        "mesh.elementset.target",
         "misc.velocity",
         "misc.detonation"  
     };
@@ -107,6 +107,14 @@ void ep16Prep::process()
     _mdb = new EXOMesh::exoMesh(exo_fname);
     _mdb->read();
 
+    // material information
+    json _matj = _jstrm["Material"];
+    for (const auto& km: _matj.object_range())
+    {
+        std::cout << toLower(km.key()) << std::endl;
+        _mat[toLower(km.key())] = km.value().as<int>();
+    }
+
     // begin processing based on order specified
     std::vector<std::string> ord = getOrder();
     for (auto oi=ord.begin(); oi!=ord.end(); oi++)
@@ -120,15 +128,19 @@ void ep16Prep::process()
             wrtPre(_tsk,__tsk);
         else if (!tsk.compare("mesh"))
             wrtMsh(_tsk,__tsk);
+        else if (!tsk.compare("misc"))
+            wrtMisc(_tsk,__tsk);
 
     }
+
 }
 
 
 void ep16Prep::wrtCmnt(std::string cmnt)
 {
-    std::string cmntStr = getCmntStr() + cmnt;
-    _write(cmntStr);
+    std::stringstream ss;
+    ss << getCmntStr() << cmnt;
+    _write(ss);
 }
 
 
@@ -139,7 +151,9 @@ void ep16Prep::wrtPre(std::string _tsk, std::string __tsk)
     if (!_tsk.compare("case"))
     {
         _tstr.clear();
+        _tstr.str(std::string());
         _tcmnt.clear();
+        _tcmnt.str(std::string());    
         int _ctype = _jstrm["Case"]["Type"].as<int>();
         int _cid =  _jstrm["Case"]["Id"].as<int>();
         std::string _des = " " + _jstrm["Case"]["Description"].as<std::string>();
@@ -151,7 +165,9 @@ void ep16Prep::wrtPre(std::string _tsk, std::string __tsk)
     else if (!_tsk.compare("run"))
     {
         _tstr.clear();
+        _tstr.str(std::string());
         _tcmnt.clear();
+        _tcmnt.str(std::string());        
         int _rmde, _unt, _pat, _dplt, _ssld;
         double _tmx, _cpmx;
         _pat = 3;
@@ -160,15 +176,15 @@ void ep16Prep::wrtPre(std::string _tsk, std::string __tsk)
         _rmde = _jstrm["Run"]["Mode"].as<int>();
         _unt = _jstrm["Run"]["Unit"].as<int>();
         _tmx = _jstrm["Run"]["Tmax"].as<double>();
-        _tcmnt << "Run Card";
+        _tcmnt << "Run Card\n";
+        _tcmnt << getCmntStr() << " RUN UNIT  PAT DPLT      TMAX     CPMAX";
         _tstr.precision(4);
         _tstr << std::setw(5) << _rmde
               << std::setw(5) << _unt
               << std::setw(5) << _pat
               << std::setw(5) << _dplt
               << std::setw(10) << std::scientific << _tmx
-              << std::setw(10) << _cpmx
-              << std::setw(5) << _ssld;
+              << std::setw(10) << _cpmx;
     }
     else if (!_tsk.compare("exodus"))
     {
@@ -205,7 +221,9 @@ void ep16Prep::wrtPre(std::string _tsk, std::string __tsk)
         _gap = 0.1;
 
         _tstr.clear();
+        _tstr.str(std::string());
         _tcmnt.clear();
+        _tcmnt.str(std::string());        
         _tcmnt << "Exodus II Description Card\n";
         _tcmnt << getCmntStr() << "NPNS NTNS NPES NTES CTCT CONV  SYM  TET       GAP";
         _tstr.precision(4);
@@ -229,64 +247,175 @@ void ep16Prep::wrtPre(std::string _tsk, std::string __tsk)
         _mxsn = _mxn;
 
         _tstr.clear();
+        _tstr.str(std::string());
         _tcmnt.clear();
+        _tcmnt.str(std::string());        
         _tcmnt << "Array Size/Dimension Card\n";
         _tcmnt << getCmntStr() << "      MXN       MXL      MXMN      MXSN";
         _tstr << std::setw(10) << _mxn
               << std::setw(10) << _mxl
               << std::setw(10) << _mxmn
-              << std::setw(10) << _mxsn
-              << std::endl;
+              << std::setw(10) << _mxsn;
     }
     // write to stream
     if (!_tcmnt.str().empty())
         wrtCmnt(_tcmnt.str());
     if (!_tstr.str().empty())
-        _write(_tstr.str());
+        _write(_tstr);
 }
 
 void ep16Prep::wrtMsh(std::string _tsk, std::string __tsk)
 {
     std::stringstream _tcmnt;
     std::stringstream _tstr;
-    if (!_tsk.compare("nodeset") && !__tsk.compare("projectile"))
+    if (!_tsk.compare("nodeset"))
     {
         _tcmnt.clear();
+        _tcmnt.str(std::string());        
         _tstr.clear();
-        _tcmnt << "Projectile Node Set Cards\n";
+        _tstr.str(std::string());
+        std::string preStr;
+        if (!__tsk.compare("projectile"))
+        {
+            _tcmnt << "Projectile Node Set Cards\n";
+            preStr = "prj";
+        }
+        else if (!__tsk.compare("target"))
+        {
+            _tcmnt << "Target Node Set Cards\n";
+            preStr = "trg";
+        }
+        else
+        {
+            std::cerr << "Error: Unknown node set type " << __tsk << "\n";
+            throw;
+        }
         _tcmnt << getCmntStr() << "NSET     XYZ";
         wrtCmnt(_tcmnt.str());
         for (int ns=0; ns<_mdb->getNumberOfNodeSet(); ns++)
         {
             std::string pre = findToStr(toLower(_mdb->getNdeSetName(ns)),"_");
-            if (!pre.compare("prj"))
+            if (!pre.compare(preStr))
             {
                 int _nset = _mdb->getNdeSetId(ns);
                 _tstr << std::setw(5) << _nset << "     000";
-                _write(_tstr.str());    
+                _write(_tstr);    
             }
         }
     }
-    else if (!_tsk.compare("nodeset") && !__tsk.compare("target"))
+    else if (!_tsk.compare("elementset"))
     {
         _tcmnt.clear();
+        _tcmnt.str(std::string());
         _tstr.clear();
-        _tcmnt << "Target Node Set Cards\n";
-        _tcmnt << getCmntStr() << "NSET     XYZ";
-        wrtCmnt(_tcmnt.str());
-        for (int ns=0; ns<_mdb->getNumberOfNodeSet(); ns++)
+        _tstr.str(std::string());
+        std::string preStr;
+        if (!__tsk.compare("projectile"))
         {
-            std::string pre = findToStr(toLower(_mdb->getNdeSetName(ns)),"_");
-            if (!pre.compare("trg"))
+            _tcmnt << "Projectile Element/Particle Set Cards\n";
+            preStr = "prj";
+        }
+        else if (!__tsk.compare("target"))
+        {
+            _tcmnt << "Target Element/Particle Set Cards\n";
+            preStr = "trg";
+        }
+        else
+        {
+            std::cerr << "Error: Unknown node set type " << __tsk << "\n";
+            throw;
+        }
+        _tcmnt << getCmntStr() << "LSET MATL TYPE SSET                                                  THICK/AREA";
+        wrtCmnt(_tcmnt.str());
+        int _lset,_matl,_type,_sset;
+        double _ta;
+        std::string _blnk;
+        _blnk.insert(_blnk.end(),50,' ');
+        for (int es=0; es<(_mdb->getNumberOfElementBlock()); es++)
+        {
+            std::string es_name = toLower(_mdb->getBlockName(es));
+            std::string pre = findToStr(es_name,"_");
+            if (!pre.compare(preStr))
             {
-                int _nset = _mdb->getNdeSetId(ns);
-                _tstr << std::setw(5) << _nset << "     000";
-                _write(_tstr.str());    
+                _lset = _mdb->getElmBlkId(es);
+                _matl = _mat[es_name];
+                if (_matl == 0)
+                {
+                    std::cerr << "Error: Keyword " << es_name << " is not defined in input file.\n";
+                    throw;
+                }
+                _type = 0;
+                _sset = 0;
+                _ta = 0.; 
+                _tstr << std::setw(5) << _lset
+                      << std::setw(5) << _matl
+                      << std::setw(5) << _type
+                      << std::setw(5) << _sset
+                      << _blnk
+                      << std::setw(10) << std::scientific << _ta;
+                _write(_tstr); 
             }
         }
-
     }
 }
 
 
+void ep16Prep::wrtMisc(std::string _tsk, std::string __tsk)
+{
+    std::stringstream _tcmnt;
+    std::stringstream _tstr;
+    if (!_tsk.compare("velocity"))
+    {
+        _tcmnt.clear();
+        _tcmnt.str(std::string());
+        _tcmnt << "Projectile and Target Velocities Card\n";
+        _tcmnt << getCmntStr() << "    PXVEL     PYVEL     PZVEL     TXVEL     TYVEL     TZVEL";
+        wrtCmnt(_tcmnt.str());
+
+        // velocity information
+        std::vector<double> _prjv,_trgv;
+        json _vj = _jstrm["Velocity"];
+        for (int i=0; i<3; i++)
+        {
+            _prjv.push_back( _vj["Projectile"][i].as<double>() );
+            _trgv.push_back( _vj["Target"][i].as<double>() );
+        }
+
+        _tstr.clear();
+        _tstr.str(std::string());
+        _tstr.precision(8);
+        _tstr << std::setw(10) << _prjv[0]
+              << std::setw(10) << _prjv[1]
+              << std::setw(10) << _prjv[2]
+              << std::setw(10) << _trgv[0]
+              << std::setw(10) << _trgv[1]
+              << std::setw(10) << _trgv[2];
+        _write(_tstr);
+    }
+    else if (!_tsk.compare("detonation"))
+    {
+        _tcmnt.clear();
+        _tcmnt.str(std::string());
+        _tcmnt << "Detonation Point Card\n";
+        _tcmnt << getCmntStr() << "     XDET      YDET      ZDET";
+        wrtCmnt(_tcmnt.str());
+
+        // detonation information
+        std::vector<double> _det;
+        json _vj = _jstrm["Detonation"];
+        for (int i=0; i<3; i++)
+        {
+            _det.push_back( _vj["Coordinate"][i].as<double>() );
+        }
+
+        _tstr.clear();
+        _tstr.str(std::string());
+        _tstr.precision(8);
+        _tstr << std::setw(10) << _det[0]
+              << std::setw(10) << _det[1]
+              << std::setw(10) << _det[2];
+        _write(_tstr);
+    }
+
+}
 #endif
