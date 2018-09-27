aece6c3e6fe01ac7a2115e3b36c6717978827e6b
twilson <twilson@illinoisrocstar.com>
2018-09-27 11:01:21 -0500

    Changed install instructions, build script, and tarball

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script will build Nemosys and all of the necessary dependencies
-# given a tarball of projects with gmsh, madlib, hdf5, cgns, netgen and vtk.
+# given a tarball of projects with gmsh, madlib, and netgen.
 # Usage: 
 #		./build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR
 
@@ -55,33 +55,3 @@ cd madlib-1.3.0/
 make -j${num_threads}
 make install
 ls Mesh/MeshDataBase.h Mesh/MeshDataBaseInterface.h Mesh/MeshDataBaseIterators.h Mesh/MeshDataBaseAttachable.h Mesh/MeshDataBaseMiniMesh.h Mesh/MshTags.h Mesh/MeshDataBaseIO.h Mesh/CheckOrientation.h Common/MAdMessage.h Common/MAdSingleton.h Geo/GmshEntities.h Geo/Physical.h Adapt/utils/NodalDataManager.h | xargs -I {} cp {} $NEMOSYS_DEPS_INSTALL_PATH/madlib/include/MAdLib
-
-# build hdf5
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
-tar zxf hdf5-1.8.21.tar.gz
-cd hdf5-1.8.21
-./configure --prefix=$NEMOSYS_DEPS_INSTALL_PATH/hdf5
-make -j${num_threads}
-make install
-
-# build cgns
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
-tar zxf cgnslib_3.2.1.tar.gz
-cd cgnslib_3.2.1
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/cgns -DCGNS_BUILD_SHARED=ON -DCGNS_BUILD_CGNSTOOLS=ON -DCGNS_ENABLE_HDF5=ON -DHDF5_INCLUDE_PATH=$NEMOSYS_DEPS_INSTALL_PATH/hdf5/include -DHDF5_LIBRARY=$NEMOSYS_DEPS_INSTALL_PATH/hdf5/lib/libhdf5.so ..
-make -j${num_threads}
-make install
-
-# build vtk
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
-tar xzf VTK-8.1.1.tar.gz
-cd VTK-8.1.1
-mkdir build
-cd build
-export CC=mpicc
-export CXX=mpicxx
-cmake -DModule_vtkParallelMPI=ON -DModule_vtkFiltersParallelMPI=ON -DModule_vtkFiltersParallelGeometry=ON -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/vtk ..
-make -j${num_threads}
-make install
