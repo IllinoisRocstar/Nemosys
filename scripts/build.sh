02124d47d1e3fddebde45b0e4fbe0ef555fe435e
Alessandro Gondolo <agondolo@illinoisrocstar.com>
2019-04-29 13:16:28 -0500

    Update build script and TPL tarball

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This script will build Nemosys and all of the necessary dependencies
-# given a tarball of projects with gmsh, madlib, hdf5, cgns, netgen and vtk.
+# given a tarball of projects with gmsh, occt, hdf5, cgns, netgen and vtk.
 # Usage:
-#		./build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR
+#    ./build.sh PATH_TO_NEMOSYS PATH_TO_NEMOSYS_TPLS_TARBALL PATH_TO_INSTALL_DIR
 
 
 set -x
@@ -17,59 +17,65 @@ NEMOSYS_TARBALL_PATH=$2
 num_threads=$(nproc)
 
 # clean
-rm -rf $NEMOSYS_DEPS_BUILD_DIR
-mkdir $NEMOSYS_DEPS_BUILD_DIR
-rm -rf $NEMOSYS_DEPS_INSTALL_PATH
+rm -rf ${NEMOSYS_DEPS_BUILD_DIR}
+mkdir ${NEMOSYS_DEPS_BUILD_DIR}
+rm -rf ${NEMOSYS_DEPS_INSTALL_PATH}
 
 # extract
-cd $NEMOSYS_DEPS_BUILD_DIR
-cp $NEMOSYS_TARBALL_PATH .
-tar zxf nemosys_tpls.tar.gz
+cd ${NEMOSYS_DEPS_BUILD_DIR}
+cp ${NEMOSYS_TARBALL_PATH} .
+tar xzf nemosys_tpls.tar.gz
 
 # build netgen
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
 tar xzf netgen-mesher-git.tar.gz
 cd netgen-mesher-git
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/netgen -DUSE_GUI=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/netgen \
+      -DUSE_GUI=OFF ..
+make -j${num_threads}
+make install
+
+# build occt
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
+tar xzf opencascade-7.3.0.tgz
+cd opencascade-7.3.0
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/opencascade \
+      -DBUILD_MODULE_Draw=OFF \
+      -DBUILD_MODULE_Visualization=OFF \
+      -DBUILD_MODULE_ApplicationFramework=OFF ..
 make -j${num_threads}
 make install
 
 # build gmsh
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
-tar zxf gmsh-2.15.0-source.tgz
-cd gmsh-2.15.0-source
-mkdir lib
-cd lib
-cmake -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/gmsh -DDEFAULT=0 -DENABLE_BUILD_LIB=1 -DENABLE_BUILD_SHARED=1 ..
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
+tar xzf gmsh-gmsh_4_2_3.tar.gz
+cd gmsh-gmsh_4_2_3
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/gmsh \
+      -DCMAKE_PREFIX_PATH=${NEMOSYS_DEPS_INSTALL_PATH}/opencascade \
+      -DENABLE_BUILD_LIB=ON -DENABLE_BUILD_SHARED=ON -DENABLE_PRIVATE_API=ON \
+      -DDEFAULT=ON -DENABLE_CGNS=OFF -DENABLE_NETGEN=OFF -DENABLE_HXT=OFF ..
 make lib shared -j${num_threads}
 make install
-cp ../Mesh/meshPartitionObjects.h $NEMOSYS_DEPS_INSTALL_PATH/gmsh/include/gmsh/
-cp ../Mesh/Generator.h $NEMOSYS_DEPS_INSTALL_PATH/gmsh/include/gmsh/
-
-# build madlib
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
-tar zxf madlib-1.3.0.tar.gz
-cd madlib-1.3.0/
-./configure --prefix=$NEMOSYS_DEPS_INSTALL_PATH/madlib --enable-moveIt --enable-benchmarks --enable-ann --enable-gmsh --with-gmsh-prefix=$NEMOSYS_DEPS_INSTALL_PATH/gmsh
-make -j${num_threads}
-make install
-ls Mesh/MeshDataBase.h Mesh/MeshDataBaseInterface.h Mesh/MeshDataBaseIterators.h Mesh/MeshDataBaseAttachable.h Mesh/MeshDataBaseMiniMesh.h Mesh/MshTags.h Mesh/MeshDataBaseIO.h Mesh/CheckOrientation.h Common/MAdMessage.h Common/MAdSingleton.h Geo/GmshEntities.h Geo/Physical.h Adapt/utils/NodalDataManager.h | xargs -I {} cp {} $NEMOSYS_DEPS_INSTALL_PATH/madlib/include/MAdLib
 
 # build vtk
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
 tar xzf vtk-7.1.0.tar.gz
 cd VTK-7.1.0
 mkdir build
 cd build
-cmake  -DCMAKE_INSTALL_PREFIX=$NEMOSYS_DEPS_INSTALL_PATH/vtk ..
+cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/vtk ..
 make -j${num_threads}
 make install
 
 # build boost
-cd $NEMOSYS_DEPS_BUILD_DIR/nemosys_tpls
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
 tar xzf boost_1_68_0.tar.gz
 cd boost_1_68_0
-./bootstrap.sh --prefix=$NEMOSYS_DEPS_INSTALL_PATH/boost
-./b2 install --prefix=$NEMOSYS_DEPS_INSTALL_PATH/boost --with=all
+./bootstrap.sh --prefix=${NEMOSYS_DEPS_INSTALL_PATH}/boost
+./b2 install --prefix=${NEMOSYS_DEPS_INSTALL_PATH}/boost --with=all
