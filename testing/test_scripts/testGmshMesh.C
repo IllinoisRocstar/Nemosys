1bf15a787177babc2b9b52aebced3944adc025a5
Alessandro Gondolo <agondolo@illinoisrocstar.com>
2019-05-15 10:08:44 -0500

    Allow setting MSH version and binary/ASCII option.

--- testing/test_scripts/testGmshMesh.C
+++ testing/test_scripts/testGmshMesh.C
@@ -1,13 +1,14 @@
 #include <gmshMesh.H>
 #include <gtest.h>
 
-std::string arg_fname;
+std::string arg_fnameMSH2;
+std::string arg_fnameMSH4;
+std::string arg_fnameMSH41;
 
 // Aux functions
-int compareFiles(const std::string &fn1, const std::string &fn2)
+bool filesEqual(const std::string &fn1, const std::string &fn2)
 {
   fstream f1, f2;
-  int pass = 1;
   f1.open(fn1, fstream::in);
   f2.open(fn2, fstream::in);
   char string1[256], string2[256];
@@ -20,11 +21,10 @@ int compareFiles(const std::string &fn1, const std::string &fn2)
       cout << j << "-the strings are not equal" << "\n";
       cout << "   " << string1 << "\n";
       cout << "   " << string2 << "\n";
-      pass = 0;
-      break;
+      return false;
     }
   }
-  return pass;
+  return true;
 }
 
 int test_gmshMeshDefaultConstructor() {
@@ -48,13 +48,31 @@ int test_gmshMeshFilenameConstructor(const std::string &fname) {
   return 0;
 }
 
-int test_gmshMeshWrite(const std::string &fname) {
+int test_gmshMeshWriteMSH2(const std::string &fname) {
   std::string test_fname = "test_" + fname;
   gmshMesh gmshMesh1 = gmshMesh(fname);
 
-  gmshMesh1.write(test_fname);
+  gmshMesh1.write(test_fname, 2.0, false);
 
-  return 0;
+  return filesEqual(fname, test_fname);
+}
+
+int test_gmshMeshWriteMSH4(const std::string &fname) {
+  std::string test_fname = "test_" + fname;
+  gmshMesh gmshMesh1 = gmshMesh(fname);
+
+  gmshMesh1.write(test_fname, 4.0, false);
+
+  return filesEqual(fname, test_fname);
+}
+
+int test_gmshMeshWriteMSH41(const std::string &fname) {
+  std::string test_fname = "test_" + fname;
+  gmshMesh gmshMesh1 = gmshMesh(fname);
+
+  gmshMesh1.write(test_fname, 4.1, false);
+
+  return filesEqual(fname, test_fname);
 }
 
 // TEST macros
@@ -65,20 +83,32 @@ TEST(gmshMesh, DefaultConstructor)
 
 TEST(gmshMesh, FilenameConstructor)
 {
-  EXPECT_EQ(0, test_gmshMeshFilenameConstructor(arg_fname));
+  EXPECT_EQ(0, test_gmshMeshFilenameConstructor(arg_fnameMSH41));
 }
 
-TEST(gmshMesh, Write)
+TEST(gmshMesh, WriteMSH2)
 {
-  EXPECT_EQ(0, test_gmshMeshWrite(arg_fname));
+  EXPECT_EQ(1, test_gmshMeshWriteMSH2(arg_fnameMSH2));
+}
+
+TEST(gmshMesh, WriteMSH4)
+{
+  EXPECT_EQ(1, test_gmshMeshWriteMSH4(arg_fnameMSH4));
+}
+
+TEST(gmshMesh, WriteMSH41)
+{
+  EXPECT_EQ(1, test_gmshMeshWriteMSH41(arg_fnameMSH41));
 }
 
 // test constructor
 int main(int argc, char** argv) {
   // IO
   ::testing::InitGoogleTest(&argc, argv);
-  assert(argc == 2);
-  arg_fname = argv[1];
+  assert(argc == 4);
+  arg_fnameMSH2 = argv[1];
+  arg_fnameMSH4 = argv[2];
+  arg_fnameMSH41 = argv[3];
 
   // run tests
   int res = RUN_ALL_TESTS();
