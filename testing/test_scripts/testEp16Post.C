4b31f300071444dc27c38fb350dcc02d7d6f71d5
Masoud Safdari <msafdari@illinoisrocstar.com>
2019-08-02 08:28:37 -0500

    Kmeans and quickhull

---
+++ testing/test_scripts/testEp16Post.C
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <cmath>
+#include "ep16Post.H"
+#include "point.H"
+
+
+// data loading
+class TestDriver : public testing::Test 
+{
+  protected:
+  jsoncons::json inp;
+
+  virtual void SetUp() 
+  {
+    std::ifstream inputStream;
+    inputStream.open("ep16post.json");
+    inputStream >> inp;
+  }
+};
+
+TEST_F(TestDriver, EndToEnd) 
+{
+  int ret = 0;
+  NEM::EPC::ep16Post* ep = NEM::EPC::ep16Post::readJSON(inp, ret);
+  ASSERT_EQ(0, ret);
+  std::vector<Point> means(ep->kmeans->getMeans());
+  ASSERT_EQ(4, means.size());
+}
+
+int main(int argc, char **argv) 
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
