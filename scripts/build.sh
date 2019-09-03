7463da7d6368d98bcd7839ef0d1a98c650637b25
Alessandro Gondolo <agondolo@illinoisrocstar.com>
2019-09-03 15:47:09 -0500

    Add cfmesh to contrib. Taken from git@git.illinois.rocstar:apatel/cfmesh_multimaterialmeshing.git @ 99005bccfb078660d737d705fe2717c02081a965

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,64 +18,66 @@ num_threads=$(nproc)
 # clean
 rm -rf ${NEMOSYS_DEPS_BUILD_DIR}
 mkdir ${NEMOSYS_DEPS_BUILD_DIR}
-rm -rf ${NEMOSYS_DEPS_INSTALL_PATH}
+rm -rf "${NEMOSYS_DEPS_INSTALL_PATH}"
 
 # extract
-cd ${NEMOSYS_DEPS_BUILD_DIR}
-cp ${NEMOSYS_TARBALL_PATH} .
+cd ${NEMOSYS_DEPS_BUILD_DIR} || exit
+cp "${NEMOSYS_TARBALL_PATH}" .
 tar xzf nemosys_tpls.tar.gz
 
+# build boost
+if [[ $(ldconfig -p | grep libboost) -eq 0 ]]; then
+  cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls || exit
+  tar xzf boost_1_68_0.tar.gz
+  cd boost_1_68_0 || exit
+  ./bootstrap.sh --prefix="${NEMOSYS_DEPS_INSTALL_PATH}"/boost
+  ./b2 install --prefix="${NEMOSYS_DEPS_INSTALL_PATH}"/boost --with=all
+fi
+
 # build netgen
-cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls || exit
 tar xzf netgen-mesher-git.tar.gz
-cd netgen-mesher-git
+cd netgen-mesher-git || exit
 mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/netgen \
+cd build || exit
+cmake -DCMAKE_INSTALL_PREFIX="${NEMOSYS_DEPS_INSTALL_PATH}"/netgen \
       -DUSE_GUI=OFF ..
-make -j${num_threads}
+make -j"${num_threads}"
 make install
 
 # build occt
-cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls || exit
 tar xzf opencascade-7.3.0.tgz
-cd opencascade-7.3.0
+cd opencascade-7.3.0 || exit
 mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/opencascade \
+cd build || exit
+cmake -DCMAKE_INSTALL_PREFIX="${NEMOSYS_DEPS_INSTALL_PATH}"/opencascade \
       -DBUILD_DOC_Overview=OFF \
       -DBUILD_MODULE_Draw=OFF \
       -DBUILD_MODULE_Visualization=OFF \
       -DBUILD_MODULE_ApplicationFramework=OFF ..
-make -j${num_threads}
+make -j"${num_threads}"
 make install
 
 # build gmsh
-cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls || exit
 tar xzf gmsh-gmsh_4_2_3.tar.gz
-cd gmsh-gmsh_4_2_3
+cd gmsh-gmsh_4_2_3 || exit
 mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/gmsh \
-      -DCMAKE_PREFIX_PATH=${NEMOSYS_DEPS_INSTALL_PATH}/opencascade \
+cd build || exit
+cmake -DCMAKE_INSTALL_PREFIX="${NEMOSYS_DEPS_INSTALL_PATH}"/gmsh \
+      -DCMAKE_PREFIX_PATH="${NEMOSYS_DEPS_INSTALL_PATH}"/opencascade \
       -DENABLE_BUILD_LIB=ON -DENABLE_BUILD_SHARED=ON -DENABLE_PRIVATE_API=ON \
       -DDEFAULT=ON -DENABLE_CGNS=OFF -DENABLE_NETGEN=OFF -DENABLE_FLTK=OFF -DENABLE_HXT=OFF ..
-make lib shared -j${num_threads}
+make lib shared -j"${num_threads}"
 make install
 
 # build vtk
-cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
+cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls || exit
 tar xzf vtk-7.1.0.tar.gz
-cd VTK-7.1.0
+cd VTK-7.1.0 || exit
 mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${NEMOSYS_DEPS_INSTALL_PATH}/vtk ..
-make -j${num_threads}
+cd build || exit
+cmake -DCMAKE_INSTALL_PREFIX="${NEMOSYS_DEPS_INSTALL_PATH}"/vtk ..
+make -j"${num_threads}"
 make install
-
-# build boost
-cd ${NEMOSYS_DEPS_BUILD_DIR}/nemosys_tpls
-tar xzf boost_1_68_0.tar.gz
-cd boost_1_68_0
-./bootstrap.sh --prefix=${NEMOSYS_DEPS_INSTALL_PATH}/boost
-./b2 install --prefix=${NEMOSYS_DEPS_INSTALL_PATH}/boost --with=all
